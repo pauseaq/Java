88495b437d134a4910dccedb80854228404cdad6
asdf <126747138+pauseaq@users.noreply.github.com>
2023-07-06 12:23:44 +0900

    Create WhileTest1.java

---
+++ src/day03/WhileTest1.java
@@ -0,0 +1,14 @@
+package day03;
+
+public class WhileTest1 {
+	public static void main(String[] args) {
+		
+		int i = 10;
+		while(i > 0) {
+			System.out.print(i);
+			System.out.print(' ');
+			i--;
+		}
+	}
+
+}
